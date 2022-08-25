25ba89806e9eb7abfd9570c970a71b1c15c77a3d
Phuc Nguyen <nguyenphuc2002ok@gmail.com>
2022-08-25 21:45:06 +0700

    Dựng khung Layout mặc định

--- src/Components/Layout/components/Header/index.js
+++ src/Components/Layout/components/Header/index.js
@@ -1,5 +1,15 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+const cx = classNames.bind(styles);
 function Header() {
-  return <h2>Header</h2>;
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        {/* {logo} */}
+        {/* {search} */}
+      </div>
+    </header>
+  );
 }
 
 export default Header;
